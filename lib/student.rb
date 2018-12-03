13e82ea669c50b8c187f5ee373ba09fdae892c06
github username <github email address>
2018-12-03 12:35:19 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,29 +1,109 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize(id=nil, name=nil, grade=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT "#{x}"
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+    .first
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = "#{x}"
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    Student.new(row[0], row[1], row[2])
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
     # remember each row should be a new instance of the Student class
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = "#{name}"
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
